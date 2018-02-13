ab03ee9ad5be456b26967a8cba0af4795fb46748
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-02-13 12:13:17 -0800

    fixed minor bugs to get code running

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Provider } from "react-native";
+import { Provider } from "react-redux";
 
-import { Store } from "./src/components";
-import { Header } from "./src/components";
+import Store from "./src/Store";
+import { Header, CryptoContainer } from "./src/components";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Header />
-        <CryptoContainer />
-      </View>
+      <Provider store={Store}>
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
