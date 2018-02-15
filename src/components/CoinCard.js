b189549319ee7bd8ba7ddf9383fba5736154e7b9
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-02-15 14:51:44 -0800

    updated CoinCard view and added styles

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { images } from "../Utils/CoinIcons";
 
-const CoinCard = props => {
+const styles = StyleSheet.create({});
+
+const { container, image, bold } = styles;
+
+const CoinCard = ({
+  symbol,
+  coin_name,
+  price_usd,
+  percent_change_24h,
+  percent_change_7d
+}) => {
   return (
-    <View>
-      <Text>{props.symbol}</Text>
-      <Text>{props.price_usd}</Text>
-      <Text>{props.percent_change_24th}</Text>
+    <View style={container}>
+      <Image style={styles.image} source={{ uri: images[symbol] }} />
+      <Text>{symbol}</Text>
+      <Text>{coin_name}</Text>
+      <Text>
+        {price_usd}
+        <Text style={bold}>$</Text>
+      </Text>
+      <Text>Change past 24 hours: {percent_change_24h}</Text>
+      <Text>Change past 7 days: {percent_change_7d}</Text>
     </View>
   );
 };
